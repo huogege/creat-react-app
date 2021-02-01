7a21b2a7972f178f1ab773d7012e12974bfcb306
zhangyong <zhangyong.cq2@chinaccs.cn>
2021-02-01 17:23:07 +0800

    fix

--- src/routes/currrent-tick.js
+++ src/routes/currrent-tick.js
@@ -1,3 +1,7 @@
+/**
+ * 在短时间内，比如一个事件循环中，浏览器会把对同一个dom的操作合并，所以原生和react去设置背景只生效了一次。
+ * 纯js操作dom是同步的，但是渲染是异步的，因为js进程和gpu进程是互斥的。
+ */
 import React,{useState} from 'react'
 import resolve from 'resolve'
  const MyTick = (props) => {
@@ -5,8 +9,10 @@ import resolve from 'resolve'
      const onClick =() => {
         console.log(this)
         const box = document.getElementById('box')
-        //box.style.backgroundColor = 'blue'
+        box.style.backgroundColor = 'blue'
+        console.log(box.style.backgroundColor);   
         setstyle({color:'red'})
+        console.log(style)
         for(let i=0;i<2000000000;i++) {
             if(i == 199999999) {
                 console.log(i)
